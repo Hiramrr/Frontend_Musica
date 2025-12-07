b9aa606381e03e76ccff621b06d99c7ad8f842b1
rodrigoHol <rodrigokum091@gmail.com>
2025-12-07 14:22:49 -0600

    Creando infraestructura de conexion con el back

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+import apiClient from '@/api/axios'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+console.log('--- INICIANDO PRUEBA DE CONEXIÓN ---');
+apiClient.get('/artistas')
+  .then(response => {
+    console.log('✅ CONEXIÓN EXITOSA! Datos recibidos:');
+    console.log(response.data); // Aquí se imprimirá tu JSON
+  })
+  .catch(error => {
+    console.error('❌ ERROR DE CONEXIÓN:', error);
+  });
