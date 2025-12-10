d85bec543402ffade7fc10c0c24be8e36b159854
rodrigoHol <rodrigokum091@gmail.com>
2025-12-09 18:36:57 -0600

    Debuggin

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import CrearCuentaView from '../views/CrearCuentaView.vue'
